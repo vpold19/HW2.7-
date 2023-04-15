ed9216080c64a5369a0ff17ce4d08a922d064cbd
vadim <vadimpoldolin19@gmail.com>
2023-04-15 22:00:51 +0200

    First commit

---
+++ src/test/java/com/example/vadim/collection/hwcollectionsvarietyofmplementations/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vadim.collection.hwcollectionsvarietyofmplementations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
